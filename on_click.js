4026541c30abdb3e37a8b42fe3f1e212d0bb17f4
zeted0410 <148453706+zeted0410@users.noreply.github.com>
2024-06-07 23:13:37 +0930

    Add files via upload

---
+++ on_click.js
@@ -0,0 +1,6 @@
+document.getElementById('button_login').addEventListener('click', (event) => {
+    window.open("play.html");
+})
+document.getElementById('button_signup').addEventListener('click', (event) => {
+    window.open("explain.html");
+})
